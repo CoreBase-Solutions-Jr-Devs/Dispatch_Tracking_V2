c5b2d13c4d3a63c2ac8cd445f07b2d831b901f3f
mikewanjero <mikewanj@gmail.com>
2025-08-25 12:57:33 +0300

    {FEAT} delivery status cards

--- src/pages/overview/_component/status-cards/delivery-status-cards.jsx
+++ src/pages/overview/_component/status-cards/delivery-status-cards.jsx
@@ -2,21 +2,17 @@ import React from 'react';
 import SharedStatusCard from './shared-status-cards';
 
 const DeliveryStatusCards = ({ data }) => {
-    const today = new Date().toDateString();
-    
     const statusCounts = {
-        todaysInvoices: data.filter((i) => new Date(i.createdAt).toDateString() === today).length,
-        pending: data.filter((i) => i.status === "In Progress").length,
-        delivered: data.filter((i) => i.status === "Delivered").length,
-        avgDeliveryTime: "3hr 33min",
+        PendingCollection: data.filter((i) => i.status === "Pending Collection").length,
+        InProgress: data.filter((i) => i.status === "In Progress").length,
+        Delivered: data.filter((i) => i.status === "Delivered").length,
     }
 
     return (
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
-            <SharedStatusCard status="Store" label="Today's Deliveries" count={statusCounts.todaysInvoices} />
-            <SharedStatusCard status="Verification" label="Invoices Pending Collection" count={statusCounts.pending} />
-            <SharedStatusCard status="Dispatch" label="Invoices Delivered" count={statusCounts.delivered} />
-            <SharedStatusCard status="Delivered" label="Avg Delivery Time" count={statusCounts.avgDeliveryTime} />
+            <SharedStatusCard status="Pending Collection" label="Pending Collection" count={statusCounts.PendingCollection} />
+            <SharedStatusCard status="In Progress" label="In Progress" count={statusCounts.InProgress} />
+            <SharedStatusCard status="Delivered" label="Delivered" count={statusCounts.Delivered} />
         </div>
     );
 }
