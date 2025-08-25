9b11390986ab2723e76d3cec5a6e688ab07b8a8a
Essy116 <githinjie40@gmail.com>
2025-08-25 11:58:23 +0300

    feat(bootstrap): add verifier and store pages with modal popups and grid

---
+++ src/pages/bootstrap/verifier-edit.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const verifier-edit = () => {
+  return (
+    <div>verifier-edit</div>
+  )
+}
+
+export default verifier-edit
---
+++ src/pages/bootstrap/verifier.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const verifier = () => {
+  return <div></div>;
+};
+
+export default verifier;
