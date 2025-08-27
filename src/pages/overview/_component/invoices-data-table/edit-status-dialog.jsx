bc50138e04553a998106f4c7c61f18ee612eb76c
mikewanjero <mikewanj@gmail.com>
2025-08-27 08:47:02 +0300

    {Feat} components into `Dialog`

--- src/pages/overview/_component/invoices-data-table/edit-status-dialog.jsx
+++ src/pages/overview/_component/invoices-data-table/edit-status-dialog.jsx
@@ -2,6 +2,12 @@
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription, DialogTrigger, DialogFooter } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
+import { Separator } from "@/components/ui/separator";
+import { Badge } from "@/components/ui/badge";
+import { Label } from "@radix-ui/react-label";
+import { Input } from "@/components/ui/input";
+import { Form } from "@/components/ui/form";
+import { Textarea } from "@/components/ui/textarea";
 
 const EditStatusDialog = ({ children, rowData, onSubmit }) => {
     const [isOpen, setIsOpen] = useState(false);
@@ -22,27 +28,90 @@ const EditStatusDialog = ({ children, rowData, onSubmit }) => {
     return (
         <Dialog open={isOpen} onOpenChange={handleOpenChange}>
             <DialogTrigger asChild>{children}</DialogTrigger>
-            <DialogContent className="sm:max-w-md">
-                <DialogHeader>
-                    <DialogTitle>Edit Status</DialogTitle>
+            <DialogContent className="sm:max-w-3xl">
+                <DialogHeader className='flex flex-row justify-between items-center mt-3'>
+                    <DialogTitle className="text-orange-300">VERIFICATION - 1</DialogTitle>
+                    <Badge variant="primary" className="uppercase bg-green-300">Document Verified</Badge>
                 </DialogHeader>
 
-                {/* Form to nbe inserted here */}
-                <div className="my-4">
-                    <p className="text-muted-foreground text-sm">
-                        Pop Up Form
-                    </p>
+                {/* Form to be inserted here */}
+                <div className="my-2">
+                    <Separator className="mb-4"/>
+                    <Form className="flex flex-col gap-2">
+                        <section className="flex flex-row justify-items-start items-center gap-x-12 mb-2">
+                            <Label className="text-xs font-medium">Customer Name:</Label>
+                            <Label className="text-xs font-medium"> BADAR PHARMACY JOMO KENYARRA AVE.</Label>
+                        </section>
+                        <section className="flex flex-row justify-between items-center gap-x-6 mb-2 text-wrap">
+                            <div className="flex justify-between w-1/2">
+                                <Label className="text-xs font-medium">Invoice No:</Label>
+                                <Label className="text-xs font-medium"> W1-000123</Label>
+                            </div>
+                            <div className="flex justify-between w-1/2">
+                                <Label className="text-xs font-medium">Invoice Date & Time:</Label>
+                                <Label className="text-xs font-medium"> 26-08-2025 08:59 AM</Label>
+                            </div>
+                        </section>
+                        <section className="flex flex-row justify-between items-center gap-x-6 mb-2 text-wrap">
+                            <div className="flex justify-between w-1/2">
+                                <Label className=" text-xs font-medium">Salesman:</Label>
+                                <Label className="text-xs font-medium"> Samir</Label>
+                            </div>
+                            <div className="flex justify-between w-1/2">
+                                <Label className="text-xs font-medium">Start Date and Time:</Label>
+                                <Label className="text-xs font-medium"> 27-08-2025 09:15 AM</Label>
+                            </div>
+                        </section>
+                        <section className="flex flex-row justify-between items-center gap-x-6 text-wrap">
+                            <div className="flex justify-between w-1/2">
+                                <Label className="text-xs font-medium">Verified By:</Label>
+                                <Label className="text-xs font-medium"> PAUL NETIA</Label>
+                            </div>
+                            <div className="flex justify-between w-1/2">
+                                <Label className="text-xs font-medium">End Date and Time:</Label>
+                                <Label className="text-xs font-medium"> 27-08-2025 11:15 AM</Label>
+                            </div>
+                        </section>
+                    </Form>
+                    <Separator className="my-2"/>
+                    <section className="flex flex-row justify-between items-center mb-2">
+                        <div className="flex items-center">
+                            <Label>Items:</Label>
+                            <Label className="ml-2">5</Label>
+                        </div>
+                        <div className="flex items-center">
+                            <Label>Total Weight(kg):</Label>
+                            <Input type="number" value="12.5" className="w-20 ml-2 h-8"/>
+                        </div>
+                        <div className="flex items-center">
+                            <Label>No of Packages:</Label>
+                            <Input type="number" value="3" className="w-12 ml-2 h-8"/>
+                        </div>
+                    </section>
+                    <Label>Verification Remarks:</Label>
+                    <Textarea placeholder="Enter remarks here..." className="min-w-[80px] mb-2 bg-gray-300" />
+                    <section className="flex flex-row justify-between items-center gap-2">
+                        <section className="flex items-center gap-1">
+                            <Label htmlFor="turnaround-time" className="text-xs font-medium">Turnaround Time (mins):</Label>
+                            <Label className="text-xs font-medium uppercase">0d 02h 30m 0s</Label>
+                        </section>
+                        <section className="flex items-center gap-1">
+                            <Label htmlFor="verificationControl" className="text-xs font-medium">Verification Control:</Label>
+                            <Label className="text-xs font-medium uppercase">CoreDBA</Label>
+                        </section>
+                    </section>
                 </div>
 
-                <DialogFooter>
+                <DialogFooter className="flex flex-row justify-end">
+                    <Button variant="outline" onClick={handleDialogClose} className="mt-2 mr-2 bg-amber-600 uppercase">Start</Button>
                     <Button
                         onClick={() => {
                             if (onSubmit) onSubmit(rowData);
                             handleDialogClose();
                         }}
-                        className="w-full mt-2"
+                        className="mt-2 bg-green-500 uppercase"
                     >
-                        Save
+                        Push
                     </Button>
                 </DialogFooter>
             </DialogContent>
