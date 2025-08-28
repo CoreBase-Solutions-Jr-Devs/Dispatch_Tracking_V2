3a292fcbc16023d3dccfe31234ab720666f1fd7b
mikewanjero <mikewanj@gmail.com>
2025-08-28 23:59:24 +0300

    {FIX} semantics

--- src/pages/overview/_component/invoices-data-table/edit-status-dialog/Delivery/meta.jsx
+++ src/pages/overview/_component/invoices-data-table/edit-status-dialog/Delivery/meta.jsx
@@ -6,7 +6,7 @@ export default function DeliveryMeta() {
         <div className='flex flex-col border-b pb-4'>
             <section className="flex flex-row justify-between gap-2">
                 <section className="flex items-center gap-1">
-                    <Label htmlFor="turnaround-time" className="text-xs font-medium">Turnaround Time (mins):</Label>
+                    <Label htmlFor="turnaround-time" className="text-xs font-medium">Turnaround Time:</Label>
                     <Label className="text-xs font-medium uppercase">0d 02h 30m 0s</Label>
                 </section>
                 <section className="flex items-center gap-1">
