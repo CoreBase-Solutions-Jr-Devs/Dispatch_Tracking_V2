d2267a4356bee53d4a5e67aa961d64dc7e876c0d
mikewanjero <mikewanj@gmail.com>
2025-08-31 22:36:04 +0300

    {FEAT} `Dropdown` to `Select`

--- src/pages/overview/_component/invoices-data-table/edit-status-dialog/Delivery/summary.jsx
+++ src/pages/overview/_component/invoices-data-table/edit-status-dialog/Delivery/summary.jsx
@@ -1,62 +1,57 @@
-import { Button } from '@/components/ui/button'
-import { DropdownMenu, DropdownMenuContent, DropdownMenuLabel, DropdownMenuRadioGroup, DropdownMenuRadioItem, DropdownMenuSeparator, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
+import { useState } from 'react'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
-import React from 'react'
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
 
 export default function DeliverySummary() {
-    return (
-        <div className='flex flex-col'>
-            <section className='flex flex-row justify-between items-center gap-6 mb-2'>
-                <div className='flex justify-between w-1/2'>
-                    <Label className="text-xs font-medium">Delivery Status:</Label>
-                    <DropdownMenu>
-                        <DropdownMenuTrigger asChild>
-                            <Button variant="dispatch" className="bg-gray-300 hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none">
-                                Status
-                            </Button>
-                        </DropdownMenuTrigger>
-                        <DropdownMenuContent className="w-40 bg-gray-200">
-                            <DropdownMenuLabel>Current Status</DropdownMenuLabel>
-                            <DropdownMenuSeparator />
+    const [status, setStatus] = useState("")
+    const [mode, setMode] = useState("")
 
-                            <DropdownMenuRadioGroup value="Delivered" onValueChange={(value) => console.log(value)}>
-                                <DropdownMenuRadioItem value="Pending Collection">Pending Collection</DropdownMenuRadioItem>
-                                <DropdownMenuRadioItem value="In Progress">In Progress</DropdownMenuRadioItem>
-                                <DropdownMenuRadioItem value="Delivered">Delivered</DropdownMenuRadioItem>
-                            </DropdownMenuRadioGroup>
-                        </DropdownMenuContent>
-                    </DropdownMenu>
+    return (
+        <div className="flex flex-col">
+            {/* Delivery Status */}
+            <section className="flex flex-row justify-between items-center gap-6 mb-2">
+                <div className="flex justify-between w-1/2">
+                <Label className="text-xs font-medium">Delivery Status:</Label>
+                <Select value={status} onValueChange={setStatus}>
+                    <SelectTrigger className="w-full border border-gray-300 rounded-md px-2">
+                        <SelectValue placeholder="Select Mode" />
+                    </SelectTrigger>
+
+                    <SelectContent className="w-40 bg-gray-200">
+                        <SelectItem value="pending">Pending Collection</SelectItem>
+                        <SelectItem value="progress">In Progress</SelectItem>
+                        <SelectItem value="delivered">Delivered</SelectItem>
+                    </SelectContent>
+                </Select>
                 </div>
-                <div className='flex justify-between w-1/2'>
+                <div className="flex justify-between w-1/2">
                     <Label className="text-xs font-medium">Invoice Value:</Label>
-                    <Label className="text-xs font-medium"> KES 14,700.00</Label>
+                    <Label className="text-xs font-medium">KES 14,700.00</Label>
                 </div>
             </section>
-            <section className='flex flex-row justify-between items-center gap-6 mb-4'>
-                <div className='flex justify-between w-1/2'>
-                    <Label className="text-xs font-medium">Delivery Mode:</Label>
-                    <DropdownMenu>
-                        <DropdownMenuTrigger asChild>
-                            <Button variant="dispatch" className="bg-gray-300 hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none">
-                                Mode
-                            </Button>
-                        </DropdownMenuTrigger>
-                        <DropdownMenuContent className="w-40 bg-gray-200">
-                            <DropdownMenuLabel>Current Status</DropdownMenuLabel>
-                            <DropdownMenuSeparator />
 
-                            <DropdownMenuRadioGroup value="COD" onValueChange={(value) => console.log(value)}>
-                                <DropdownMenuRadioItem value="COD">Cash on Order</DropdownMenuRadioItem>
-                                <DropdownMenuRadioItem value="COO">Cash on Delivery</DropdownMenuRadioItem>
-                                <DropdownMenuRadioItem value="Account">Account</DropdownMenuRadioItem>
-                            </DropdownMenuRadioGroup>
-                        </DropdownMenuContent>
-                    </DropdownMenu>
+            {/* Delivery Mode */}
+            <section className="flex flex-row justify-between items-center gap-6 mb-4">
+                <div className="flex justify-between w-1/2">
+                    <Label className="text-xs font-medium">Delivery Mode:</Label>
+                    <Select value={mode} onValueChange={setMode}>
+
+                        <SelectTrigger className="w-full border border-gray-300 rounded-md px-2">
+                            <SelectValue placeholder="Select Mode" />
+                        </SelectTrigger>
+
+                        <SelectContent className="w-40 bg-gray-200">
+                            <SelectItem value="COO">Cash on Order</SelectItem>
+                            <SelectItem value="COD">Cash on Delivery</SelectItem>
+                            <SelectItem value="ACC">Account</SelectItem>
+                        </SelectContent>
+
+                    </Select>
                 </div>
-                <div className='flex justify-between w-1/2'>
+                <div className="flex justify-between w-1/2">
                     <Label className="text-xs font-medium">Cash Collected:</Label>
-                    <Input type="number" value="0.00" className="w-32 ml-2 h-8"/>
+                    <Input type="number" value="0.00" className="w-32 ml-2 h-8" readOnly />
                 </div>
             </section>
         </div>
