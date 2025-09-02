fedfe5658c3391c950fb2019e31a2f2503b6be9b
jaywes222 <wesleyjay202@gmail.com>
2025-09-02 18:26:43 +0300

    {FIX} Project Structure

--- src/pages/overview/_component/invoices-data-table/edit-status-dialog/Dispatch/meta.jsx
+++ src/pages/overview/_component/invoices-data-table/edit-status-dialog/Dispatch/meta.jsx
@@ -4,19 +4,6 @@ export default function DispatchMeta() {
   return (
     <section className="flex flex-row justify-between items-center gap-2">
       <section className="flex items-center gap-1">
-        <Label className="text-xs font-medium">Dispatch Control:</Label>
-        <Label className="text-xs font-medium uppercase">CoreDBA</Label>
-      </section>
-    </section>
-  );
-}
-
-import { Label } from "@/components/ui/label";
-
-export default function DispatchMeta() {
-  return (
-    <section className="flex flex-row justify-between items-center gap-2">
-      <section className="flex items-center gap-1">
         <Label className="text-xs font-medium">Turnaround Time (mins):</Label>
         <Label className="text-xs font-medium text-muted uppercase">
           0d 02h 30m 0s
