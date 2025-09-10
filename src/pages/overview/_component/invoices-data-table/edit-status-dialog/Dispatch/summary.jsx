6519b929b180f395afc2846c2f72a8066a2ce238
mikewanjero <mikewanj@gmail.com>
2025-09-10 18:48:31 +0300

    {FEAT} `helper fns`

--- src/pages/overview/_component/invoices-data-table/edit-status-dialog/Dispatch/summary.jsx
+++ src/pages/overview/_component/invoices-data-table/edit-status-dialog/Dispatch/summary.jsx
@@ -1,15 +1,21 @@
 import { Label } from "@/components/ui/label";
 
-export default function DispatchSummary({ data }) {
+export default function DispatchSummary({ data = [] }) {
+  const totalCount = data.length; // Document count
+  const totalValue = data.reduce((sum, doc) => {
+    const cleanAmount = parseFloat((doc.amount || "0").toString().replace(/,/g, "")); // Remove commas
+    return sum + (isNaN(cleanAmount) ? 0 : cleanAmount); // 0 value if not a number
+  },0).toFixed(2); // 2DP
+
   return (
     <section className="flex flex-row  justify-end items-center mb-2">
       <div className="flex items-center  text-xs text-medium mr-4">
         <Label>Total Count:</Label>
-        <Label className="ml-2">2</Label>
+        <Label className="ml-2">{totalCount}</Label>
       </div>
       <div className="flex items-center text-xs text-medium">
         <Label>Total Value:</Label>
-        <Label className="ml-2">1,470.00</Label>
+        <Label className="ml-2">{totalValue}</Label>
       </div>
     </section>
   );
