faa630fabb6e927bdd084493ca3295a573aa2c68
mikewanjero <mikewanj@gmail.com>
2025-09-10 19:12:44 +0300

    {FIX} dropdown placement `collection type`

--- src/pages/overview/_component/invoices-data-table/edit-status-dialog/Dispatch/select.jsx
+++ src/pages/overview/_component/invoices-data-table/edit-status-dialog/Dispatch/select.jsx
@@ -9,6 +9,22 @@ export default function DispatchSelect({ values, onChange }) {
   return (
     <div className="flex flex-col gap-2 text-xs font-medium">
       <div className="flex justify-between items-center">
+        <label className="text-xs font-medium">Collection Type:</label>
+        <Select
+          value={values.collectionType}
+          onValueChange={(val) => onChange("collectionType", val)}
+        >
+          <SelectTrigger className="w-32 !h-7 border border-gray-300 rounded-md px-1 ml-1 text-xs font-medium">
+            {values.collectionType}
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="self-collection">Self-Collection</SelectItem>
+            <SelectItem value="delivery">Delivery</SelectItem>
+          </SelectContent>
+        </Select>
+      </div>
+      
+      <div className="flex justify-between items-center">
         <label className="text-xs font-medium">Delivery Person:</label>
         <Select
           value={values.deliveryPerson}
@@ -56,21 +72,6 @@ export default function DispatchSelect({ values, onChange }) {
         </Select>
       </div>
 
-      <div className="flex justify-between items-center">
-        <label className="text-xs font-medium">Collection Type:</label>
-        <Select
-          value={values.collectionType}
-          onValueChange={(val) => onChange("collectionType", val)}
-        >
-          <SelectTrigger className="w-32 !h-7 border border-gray-300 rounded-md px-1 ml-1 text-xs font-medium">
-            {values.collectionType}
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value="self-collection">Self-Collection</SelectItem>
-            <SelectItem value="delivery">Delivery</SelectItem>
-          </SelectContent>
-        </Select>
-      </div>
     </div>
   );
 }
