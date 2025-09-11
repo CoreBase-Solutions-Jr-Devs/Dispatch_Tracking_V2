364732f637ac50739c2d57c41882127552579805
mikewanjero <mikewanj@gmail.com>
2025-09-12 01:16:12 +0300

    {FEAT} conditional rendering `Collection Type`

--- src/pages/overview/_component/invoices-data-table/edit-status-dialog/Dispatch/details.jsx
+++ src/pages/overview/_component/invoices-data-table/edit-status-dialog/Dispatch/details.jsx
@@ -1,28 +1,61 @@
+import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
-export default function DispatchDetails({ data }) {
+export default function DispatchDetails({ data, collectionType }) {
   return (
-    <div className="flex flex-col w-1/2 gap-5 text-xs font-medium">
+    <div className="flex flex-col w-1/2 gap-2 text-xs font-medium">
       <section className="flex justify-between items-center gap-2">
         <Label className="text-xs font-medium">Dispatch Date & Time:</Label>
         <Label className="text-xs font-medium">08/20/2025 12:31</Label>
       </section>
 
-      <section className="flex justify-between">
-        <div className="flex items-center gap-2 w-1/2">
-          <Label className="text-xs font-medium">DP ID:</Label>
-          <Label className="text-xs font-medium ">31456780</Label>
-        </div>
-        <div className="flex justify-end items-center gap-2 w-1/2">
-          <Label className="text-xs font-medium">DP DL:</Label>
-          <Label className="text-xs font-medium ">31456780</Label>
-        </div>
-      </section>
+      {collectionType === "self-collection" && (
+        <>
+          <section className="flex justify-between space-x-2">
+            <div className="flex items-center gap-2 w-1/2">
+              <Label className="text-xs font-medium">Client Name:</Label>
+              <Input className="w-full h-6 text-xs" placeholder="Enter Name" />
+            </div>
+            <div className="flex items-center gap-2 w-1/2">
+              <Label className="text-xs font-medium">Client ID:</Label>
+              <Input className="w-full h-6 text-xs" placeholder="Enter ID" />
+            </div>
+          </section>
 
-      <section className="flex items-center gap-2">
-        <Label className="text-xs font-medium">MAKE:</Label>
-        <Label className="text-xs font-medium ">TVS</Label>
-      </section>
+          <section className="flex items-center gap-2">
+            <Label className="text-xs font-medium">Phone No:</Label>
+            <Input className="w-full h-6 text-xs" placeholder="Enter Phone" />
+          </section>
+        </>
+      )}
+
+      {collectionType === "delivery" && (
+        <>
+          <section className="flex justify-between space-x-2">
+            <div className="flex items-center gap-2 w-1/2">
+              <Label className="text-xs font-medium">DP ID:</Label>
+              <Input className="w-full h-6 text-xs" placeholder="Driver ID" />
+            </div>
+            <div className="flex items-center gap-2 w-1/2">
+              <Label className="text-xs font-medium">DP DL:</Label>
+              <Input className="w-full h-6 text-xs" placeholder="Driver DL" />
+            </div>
+          </section>
+
+          <section className="flex justify-between gap-x-2">
+            <section className="flex items-center gap-2">
+              <Label className="text-xs font-medium">Car Make:</Label>
+              <Input className="w-full h-6 text-xs" placeholder="Car Make" />
+            </section>
+
+            <section className="flex items-center gap-2">
+              <Label className="text-xs font-medium">Reg No:</Label>
+              <Input className="w-full h-6 text-xs" placeholder="Car Plate" />
+            </section>
+          </section>
+          
+        </>
+      )}
 
     </div>
   );
