e2c47bd010152793afce0ec6297192534cd6e6b7
mikewanjero <mikewanj@gmail.com>
2025-09-12 00:17:12 +0300

    {FEAT} `meta content`

--- src/pages/overview/_component/invoices-data-table/edit-status-dialog/Dispatch/meta.jsx
+++ src/pages/overview/_component/invoices-data-table/edit-status-dialog/Dispatch/meta.jsx
@@ -2,8 +2,13 @@ import { Label } from "@/components/ui/label";
 
 export default function DispatchMeta() {
   return (
-    <section className="flex flex-row justify-between items-center gap-2">
-      <section className="flex items-center gap-1">
+    <section className="flex flex-row justify-between items-center gap-2 my-1">
+      <section className="flex items-center gap-2">
+        <Label className="text-xs text-muted font-medium">Dispatch Ref:</Label>
+        <Label className="text-xs text-muted font-medium ">DPT000000001</Label>
+      </section>
+      
+      <section className="flex items-center gap-2">
         <Label className="text-xs text-muted font-medium">Dispatch Control:</Label>
         <Label className="text-xs text-muted font-medium uppercase">CoreDBA</Label>
       </section>
