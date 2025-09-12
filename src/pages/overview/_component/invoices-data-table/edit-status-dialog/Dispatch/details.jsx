1fe0202454a4bb3c3f56a4879517c4a1fae9cbc8
mikewanjero <mikewanj@gmail.com>
2025-09-12 15:06:55 +0300

    {refactor} page layout

--- src/pages/overview/_component/invoices-data-table/edit-status-dialog/Dispatch/details.jsx
+++ src/pages/overview/_component/invoices-data-table/edit-status-dialog/Dispatch/details.jsx
@@ -4,7 +4,7 @@ import { Label } from "@/components/ui/label";
 export default function DispatchDetails({ data, collectionType }) {
   return (
     <div className="flex flex-col w-1/2 gap-2 text-xs font-medium">
-      <section className="flex justify-between items-center">
+      <section className="flex justify-between items-center h-full">
         <Label className="text-xs font-medium">Dispatch Date & Time:</Label>
         <Label className="text-xs font-medium">08/20/2025 12:31</Label>
       </section>
@@ -12,20 +12,21 @@ export default function DispatchDetails({ data, collectionType }) {
       {collectionType === "self-collection" && (
         <>
           <section className="flex justify-between space-x-2">
-            <div className="flex items-center gap-2 w-1/2">
-              <Label className="text-xs font-medium">Client Name:</Label>
-              <Input className="w-full h-6 text-xs" placeholder="Enter Name" />
+            <div className="flex flex-col items-center w-1/3">
+              <Label className="text-xs font-medium">Client Name</Label>
+              <Input className="w-full h-6 text-xs" />
             </div>
-            <div className="flex items-center gap-2 w-1/2">
-              <Label className="text-xs font-medium">Client ID:</Label>
-              <Input className="w-full h-6 text-xs" placeholder="Enter ID" />
+            <div className="flex flex-col items-center w-1/3">
+              <Label className="text-xs font-medium">Client ID</Label>
+              <Input className="w-full h-6 text-xs" />
             </div>
+            <section className="flex flex-col items-center w-1/3">
+              <Label className="text-xs font-medium">Phone No</Label>
+              <Input className="w-full h-6 text-xs" />
+            </section>
           </section>
 
-          <section className="flex items-center gap-2">
-            <Label className="text-xs font-medium">Phone No:</Label>
-            <Input className="w-full h-6 text-xs" placeholder="Enter Phone" />
-          </section>
+          
         </>
       )}
 
