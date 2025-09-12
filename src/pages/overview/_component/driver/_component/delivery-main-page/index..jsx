222e041a21d925dbffa62d5c9518075048694a4f
Essy116 <githinjie40@gmail.com>
2025-09-12 16:20:53 +0300

    style(ui): fix component alignment on page

--- src/pages/overview/_component/driver/_component/delivery-main-page/index..jsx
+++ src/pages/overview/_component/driver/_component/delivery-main-page/index..jsx
@@ -1,18 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { Separator } from "@/components/ui/separator";
-
 import DeliverySearch from "../delivery-sections/search";
 import DeliveryTable from "../delivery-sections/table";
 import DeliverySummary from "../delivery-sections/summary";
-
 import DeliveryDetails from "../delivery-sections/details";
 import DeliveryMeta from "../delivery-sections/meta";
 import DeliveryRemarks from "../delivery-sections/remarks";
 import DeliveryFooter from "../delivery-sections/footer";
-
 import DeliveryPagination from "../delivery-sections/pagination";
+
+
 export default function DeliveryInvoice({ rowData, onSubmit }) {
   const [query, setQuery] = useState("");
+ 
   return (
     <div className=" overflow-y-auto max-h-[90vh] px-2">
       <DeliverySearch
