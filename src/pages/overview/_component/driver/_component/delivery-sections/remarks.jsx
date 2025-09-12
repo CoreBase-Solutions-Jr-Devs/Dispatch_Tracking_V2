41acbd5a81cc248eed1f0f8f5b15704b813f2c3e
Essy116 <githinjie40@gmail.com>
2025-09-12 12:48:18 +0300

    feat(ui):remove  padding adjustments to reduce extra space on delivery page

--- src/pages/overview/_component/driver/_component/delivery-sections/remarks.jsx
+++ src/pages/overview/_component/driver/_component/delivery-sections/remarks.jsx
@@ -4,7 +4,7 @@ import { Textarea } from "@/components/ui/textarea";
 export default function DeliveryRemarks() {
     return (
  <div className="flex flex-col mb-2">
-  <Label className="text-xs font-medium p-2">Delivery Remarks:</Label>
+  <Label className="text-xs font-medium ">Delivery Remarks:</Label>
   <Textarea className="w-200 bg-gray-300 text-xs font-medium" />
 </div>
 
