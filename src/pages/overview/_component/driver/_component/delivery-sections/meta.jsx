9fdf5657feba43989227bfbb187af5777cefcf14
Essy116 <githinjie40@gmail.com>
2025-09-12 12:06:56 +0300

    git reset --soft HEAD~1

--- src/pages/overview/_component/driver/_component/delivery-sections/meta.jsx
+++ src/pages/overview/_component/driver/_component/delivery-sections/meta.jsx
@@ -1,12 +1,19 @@
 import { Label } from "@/components/ui/label";
 
 export default function DeliveryMeta() {
-    return (
-        <section className="flex flex-row justify-between items-center gap-2">
-            <section className="flex items-center gap-1">
-                <Label className="text-xs font-medium">Dispatch Control:</Label>
-                <Label className="text-xs font-medium uppercase">CoreDBA</Label>
-            </section>
-        </section>
-    );
+  return (
+<section className="flex flex-row gap-10 justify-between text-xs font-medium">
+  <section className="flex items-center gap-1">
+    <Label className="font-medium text-xs">Delivery Control:</Label>
+    <Label className="font-medium uppercase text-xs">CoreDBA</Label>
+  </section>
+
+  <section className="flex items-center gap-1">
+    <Label className="font-medium text-xs">Delivery Ref:</Label>
+    <Label className="font-medium text-xs">DPT000000001</Label>
+  </section>
+</section>
+
+
+  );
 }
