b4142d9d1ea823d38e459afd5eb4b0ea88845022
mikewanjero <mikewanj@gmail.com>
2025-09-15 00:48:51 +0300

    {FEAT} removing unused APIs

--- src/features/invoices/invoicesAPI.js
+++ src/features/invoices/invoicesAPI.js
@@ -122,14 +122,6 @@ export const invoicesApi = apiClient.injectEndpoints({
             }),
             invalidatesTags: ['dispatch_invoices'],
         }),
-		getDeliveryDriver: builder.query({
-			query: (userId) => ({
-				url: `/dispatch/delivery-driver`,
-				method: 'GET',
-				params: { UserId: userId },
-			}),
-			providesTags: ['delivery_driver'],
-		}),
 
 		// COLLECTION TRACKING
 		getCollectionTrackingDetails: builder.query({
@@ -217,7 +209,6 @@ export const {
 	useVerificationPushMutation,
 	// DISPATCH TRACKING
 	useFilterDispatchInvoicesMutation,
-	useGetDeliveryDriverQuery,
 	// COLLECTION TRACKING
 	useGetCollectionTrackingDetailsQuery,
 	useCollectionStartMutation,
