e64b298122db1b672b872f4e0f9fbf9e63390f7a
mikewanjero <mikewanj@gmail.com>
2025-09-16 18:27:45 +0300

    {FEAT} invoice data table - `action btns`

--- src/pages/overview/_component/dispatchPerson/dispatch-sections/search.jsx
+++ src/pages/overview/_component/dispatchPerson/dispatch-sections/search.jsx
@@ -1,12 +1,25 @@
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Search as SearchIcon } from "lucide-react";
+import EditStatusDialog from "../../invoices-data-table/edit-status-dialog/edit-status-dialog"; 
+import { useState } from "react";
 
 export default function DispatchSearch({
     value,
     onChange,
     placeholder = "Invoice No",
-    }) {
+    rowData, 
+    onSubmit, 
+}) {
+    const [startDisabled, setStartDisabled] = useState(false);
+    const [deliveryDisabled, setDeliveryDisabled] = useState(true);
+    const handleStart = () => {
+        setStartDisabled(true);
+        setDeliveryDisabled(false);
+        if (onClose) onClose();
+    };
+
     return (
         <section className="flex justify-between items-center w-full">
             <div className="relative w-1/2 max-w-sm">
@@ -20,9 +33,24 @@ export default function DispatchSearch({
                 />
             </div>
 
-            <div className="flex items-center gap-2 w-1/2 pl-4">
-                <Label className="text-xs font-medium">Dispatch No:</Label>
-                <Label className="text-xs font-medium text-muted-foreground">2</Label>
+
+            <div className="flex flex-row items-center justify-end">
+                <Button
+                    variant="verification"
+                    onClick={handleStart}
+                    disabled={startDisabled}
+                    className="mr-2 uppercase text-xs font-medium"
+                >
+                    Start
+                </Button>
+                <EditStatusDialog rowData={rowData} view="dispatch" onSubmit={onSubmit}>
+                    <Button 
+                        className="uppercase text-xs font-medium"
+                        variant="apply"
+                    >
+                        Pick Invoice
+                    </Button>
+                </EditStatusDialog>
             </div>
         </section>
     );
