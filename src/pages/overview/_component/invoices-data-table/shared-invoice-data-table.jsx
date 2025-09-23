c9c1373f89ca8ce1a91d501e59168562ec0acbbd
mikewanjero <mikewanj@gmail.com>
2025-09-23 18:56:51 +0300

    {FEAT} `IMPORTANT` added Search to shared invoice data table

--- src/pages/overview/_component/invoices-data-table/shared-invoice-data-table.jsx
+++ src/pages/overview/_component/invoices-data-table/shared-invoice-data-table.jsx
@@ -1,16 +1,17 @@
 import * as React from "react";
 import { DataTable } from "@/components/data-table";
 import { useSelector } from "react-redux";
-// import { getInvoiceFilters } from "@/components/invoice-data-table/invoice-filters";
+import { getInvoiceFilters } from "@/components/invoice-data-table/invoice-filters";
 import { getInvoiceColumns } from "@/components/invoice-data-table/invoice-columns";
 import { roleToView } from "@/lib/utils";
-// import InvoiceToolbar from "@/components/invoice-data-table/invoice-toolbar";
-// import { mockInvoices } from "@/constant/mockInvoices";
+import InvoiceToolbar from "@/components/invoice-data-table/invoice-toolbar";
+import { mockInvoices } from "@/constant/mockInvoices";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
 
 export default function SharedInvoiceDataTable() {
   const { user } = useSelector((state) => state.auth);
   const { invoices, pagination } = useSelector((state) => state.invoice);
-
   const [pages, setPages] = React.useState(
     Math.ceil(invoices.length / pagination?.pageSize) || 1
   );
@@ -29,9 +30,22 @@ export default function SharedInvoiceDataTable() {
   const handlePageChange = (data) => {
     setPageNumber(data);
   };
+  const [search, setSearch] = React.useState("");
 
   return (
     <div className="space-y-4">
+      <InvoiceToolbar role={view} />
+
+      <div className="flex-1 flex flex-col">
+        <Label className="text-xs text-muted-foreground">Search</Label>
+        <Input
+          placeholder="Invoice No / Account"
+          className="text-sm h-8 w-full sm:w-32 md:w-40 lg:w-44 xl:w-56"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
+
       <DataTable
         // data={mockInvoices}
         data={invoices}
