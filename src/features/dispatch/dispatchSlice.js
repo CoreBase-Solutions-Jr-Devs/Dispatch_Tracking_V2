5dc65953083f75446742249b7d53346c74acda94
mikewanjero <mikewanj@gmail.com>
2025-09-26 16:56:26 +0300

    {FEAT} new states and slices

--- src/features/dispatch/dispatchSlice.js
+++ src/features/dispatch/dispatchSlice.js
@@ -14,6 +14,8 @@ const initialState = {
     deliveryDriver: null,
     driverName: "",
     driverId: 0,
+    driverLicenseNo: "",
+    email: "",
     invoiceNo: 0,
     invoices: [],
     isPush: true,
@@ -25,6 +27,9 @@ const initialState = {
         pageNumber: 1,
         pageSize: 50,
     },
+    personalId: "",
+    phoneNo: "",
+    regNo: "",
     routeCode: 0,
     routeName: "",
 }
@@ -47,6 +52,16 @@ const dispatchSlice = createSlice({
             state.customerCourierPhone= action.payload.customerCourierPhone;
             state.dispatchRemarks = action.payload.dispatchRemarks;
             state.isPush = action.payload.isPush;
+        },
+        setDriverDetails:(state, action) => {
+            state.driverId = action.payload.driverId;
+            state.driverName = action.payload.driverName;
+            state.personalId = action.payload.personalId;
+            state.driverLicenseNo = action.payload.driverLicenseNo;
+            state.phoneNo = action.payload.phoneNo;
+            state.email = action.payload.email;
+            state.carMake = action.payload.carMake;
+            state.regNo = action.payload.regNo;
         },
         setInvoices: (state, action) => {
             state.invoices = action.payload.invoices;
