1b78c0ff5adbe005460101cf5f8361b89f5aff61
Essy116 <githinjie40@gmail.com>
2025-09-29 12:36:12 +0300

    refactor(search): connect search page to Redux slice

--- src/components/invoice-data-table/invoice-toolbar.jsx
+++ src/components/invoice-data-table/invoice-toolbar.jsx
@@ -8,31 +8,14 @@ import {
 import { Label } from "../ui/label";
 import { Input } from "../ui/input";
 import { Search as SearchIcon } from "lucide-react";
-import { useDispatchSearchQuery } from "@/features/dispatch/dispatchAPI";
+import { useDispatch, useSelector } from "react-redux";
+import { setSearch } from "@/features/invoices/invoiceSlice";
 
 export default function InvoiceToolbar({
   role = "admin",
   placeholder = "Invoice No,Cust Name",
+   
 }) {
-  const [searchValue, setSearchValue] = useState("");
-  const [debounceValue, setDebounceValue] = useState(searchValue);
-
-  const {
-    data: searchOptions,
-    isLoading,
-    isError,
-  } = useDispatchSearchQuery(debounceValue, {
-    skip: !debounceValue,
-  });
-
-  useEffect(() => {
-    const handler = setTimeout(() => setDebounceValue(searchValue), 1000);
-
-    return () => {
-      clearTimeout(handler);
-    };
-  }, [searchValue]);
-
   const renderSummaryCards = () => {
     switch (role.toLowerCase()) {
       case "admin":
@@ -47,22 +30,22 @@ export default function InvoiceToolbar({
         return null;
     }
   };
+  const dispatch = useDispatch();
+  const searchValue = useSelector((state) => state.invoice.search);
 
   return (
     <div className="flex justify-between items-center w-full">
-     
-      <div className="relative flex-1 flex flex-col max-w-xs">
+    <div className="relative flex-1 flex flex-col max-w-xs">
         <SearchIcon className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 h-4 w-4" />
         <Input
           type="text"
           value={searchValue}
-          onChange={(e) => setSearchValue(e.target.value)}
+          onChange={(e) => dispatch(setSearch(e.target.value))}
           placeholder={placeholder}
           className="w-full pl-9 pr-3 py-2 bg-gray-100"
         />
       </div>
 
-     
       <div className="flex items-center ml-auto">{renderSummaryCards()}</div>
     </div>
   );
