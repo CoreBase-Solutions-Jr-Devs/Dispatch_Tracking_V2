b239350b37dbd54bbd10a4e8f75cc90f530290b0
Essy116 <githinjie40@gmail.com>
2025-10-03 16:26:18 +0300

    fix(api): ensure filter option works correctly

--- src/hooks/use-role-invoice-filter.js
+++ src/hooks/use-role-invoice-filter.js
@@ -1,21 +1,27 @@
 import {
-	useFilterStoreInvoicesMutation,
-	useFilterVerificationInvoicesMutation,
-	useFilterDispatchInvoicesMutation,
-	// useFilterDeliveryInvoicesMutation,
-} from '@/features/invoices/invoicesAPI';
+  
+  useFilterDispatchInvoicesMutation,
+  // useFilterDeliveryInvoicesMutation,
+} from "@/features/invoices/invoicesAPI";
+import { useLazyGetFilteredStoreInvoicesQuery } from "@/features/store/storeAPI";
+import { useLazyGetFilteredVerificationInvoicesQuery } from "@/features/verification/verificationAPI";
 
 export function useRoleInvoiceFilter(role) {
-	switch (role) {
-		case 'store':
-			return useFilterStoreInvoicesMutation();
-		case 'verification':
-			return useFilterVerificationInvoicesMutation();
-		case 'dispatch':
-			return useFilterDispatchInvoicesMutation();
-		// case 'delivery':
-		// 	return useFilterDeliveryInvoicesMutation();
-		default:
-			throw new Error(`Unknown role: ${role}`);
-	}
+  switch (role) {
+    case "store":
+      return useLazyGetFilteredStoreInvoicesQuery(); 
+
+    case "verification":
+   
+      return useLazyGetFilteredVerificationInvoicesQuery();
+
+    case "dispatch":
+      return useFilterDispatchInvoicesMutation();
+
+    // case "delivery":
+    //   return useFilterDeliveryInvoicesMutation();
+
+    default:
+      throw new Error(`Unknown role: ${role}`);
+  }
 }
