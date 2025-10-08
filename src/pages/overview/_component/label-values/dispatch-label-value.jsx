d93c7277869ed0bdfcd456517f62fee82856bfd5
mikewanjero <mikewanj@gmail.com>
2025-10-08 18:12:09 +0300

    {FIX} aggreagted stats API

--- src/pages/overview/_component/label-values/dispatch-label-value.jsx
+++ src/pages/overview/_component/label-values/dispatch-label-value.jsx
@@ -4,50 +4,15 @@ import React, { useEffect } from "react";
 import LabelValue from "./shared-label-value";
 import { useTypedSelector } from "@/app/hook";
 import { toast } from "sonner";
+import { useGetAggregateDispatchesQuery } from "@/features/Dispmain/dispatchAPI";
 
 const DispatchLabelValue = () => {
   const { startDate, endDate, dateRange } = useTypedSelector(
     (state) => state.invoice
   );
 
-  let isLoading,
-    isError = false;
+  const { data: aggregateStats, isLoading, isError } = useGetAggregateDispatchesQuery();
 
-  let data = [];
-
-  // const [filterDispatchInvoices, { data, isLoading, isError }] =
-  //   useFilterDispatchInvoicesMutation();
-
-  // const handleApplyFilter = async () => {
-  //   const payload = {
-  //     startDate: new Date(startDate).toISOString(),
-  //     endDate: new Date(endDate).toISOString(),
-  //     dateRange,
-  //     search: "",
-  //     status: {},
-  //     pageNumber: 1,
-  //     pageSize: 50,
-  //   };
-
-  //   console.log("Filter Payload:", JSON.stringify(payload, null, 2));
-
-  //   try {
-  //     const data = await filterDispatchInvoices(payload).unwrap();
-  //     console.log(data);
-  //   } catch (error) {
-  //     let description = "error occurred. Please try again.";
-  //     if (error?.data?.errors) {
-  //       const errorMessages = Object.values(error.data.errors).flat();
-  //       if (errorMessages.length > 0) description = errorMessages.join(" ");
-  //     } else if (error?.data?.message) description = error.data.message;
-
-  //     toast.error("Invoices Failed", { description, duration: 4000 });
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   handleApplyFilter();
-  // }, []);
 
   if (isLoading) {
     return (
@@ -68,7 +33,6 @@ const DispatchLabelValue = () => {
     );
   }
 
-  const stats = data?.stats || {};
 
   return (
     <div
@@ -81,28 +45,24 @@ const DispatchLabelValue = () => {
     >
       <LabelValue
         status="Store"
-        label="Pending"
-        value={data?.invoices?.length || 0}
+        label="Total"
+        value={aggregateStats?.totalCount || 0}
       />
       <LabelValue
         status="destructive"
         label="Ongoing"
-        value={stats.pendingCount || 0}
+        value={aggregateStats?.pendingCount || 0}
       />
       <LabelValue
         status="Dispatch"
-        label="Dispatched"
-        value={stats.dispatchedCount || 0}
+        label="In Dispatch"
+        value={aggregateStats?.inDispatchCount || 0}
       />
 
       <LabelValue
         status="Delivered"
-        label="Avg. Dispatch Time"
-        value={
-          stats.avgDurationSeconds !== undefined
-            ? `${stats.avgDurationSeconds} sec`
-            : "N/A"
-        }
+        label="Dispatched"
+        value={aggregateStats?.dispatchedCount || 0}
       />
     </div>
   );
