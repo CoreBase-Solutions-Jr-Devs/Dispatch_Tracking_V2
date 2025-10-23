7b138d212da2c4485eec2f1bbe12f8c662eac607
Essy116 <githinjie40@gmail.com>
2025-10-23 07:46:07 +0300

    revert(dispatch): remove recall document functionality as it’s not suitable for current workflow

--- src/pages/overview/_component/invoices-data-table/edit-status-dialog/Dispatch/footer.jsx
+++ src/pages/overview/_component/invoices-data-table/edit-status-dialog/Dispatch/footer.jsx
@@ -82,14 +82,7 @@ export default function DispatchFooter({
       >
         Close
       </Button>
-        <Button
-        variant="destructive"
-        onClick={handleClose}
-        disabled={cancelDisabled}
-        className="mt-1 mr-2 uppercase text-xs font-medium"
-      >
-        Recall
-      </Button>
+     
     </div>
   );
 }
