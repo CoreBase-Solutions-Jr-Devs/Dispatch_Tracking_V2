fdb540648862bea93b3fe53779bc05e20ea94980
Netia Paul <netiapaul23@gmail.com>
2025-10-24 15:48:44 +0300

    {FEAT} recall function and button added {FEAT} update invoice to the slice {FIX} removes the dispatch number if the dispatchnumber is 0

--- src/pages/overview/_component/invoices-data-table/edit-status-dialog/Dispatch/footer.jsx
+++ src/pages/overview/_component/invoices-data-table/edit-status-dialog/Dispatch/footer.jsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
-import { useSaveSelectionsMutation } from "@/features/dispatch/dispatchAPI";
+import {
+  useCreateDispatchesMutation,
+  useRecallInvoicesMutation,
+  useSaveSelectionsMutation,
+} from "@/features/dispatch/dispatchAPI";
 import { toast } from "sonner";
 import { setSelectedDipatches } from "@/features/dispatch/dispatchSlice";
 import { useAppDispatch, useTypedSelector } from "@/app/hook";
@@ -8,10 +12,12 @@ import EditStatusDialog from "../edit-status-dialog";
 
 export default function DispatchFooter({
   rowData,
+  selectedRow = {},
   selectedDocs = [],
   onSubmit,
   selectValues,
   onClose,
+  handleRowSelect,
 }) {
   const [startDisabled, setStartDisabled] = useState(true);
   const [deliveryDisabled, setDeliveryDisabled] = useState(true);
@@ -19,10 +25,15 @@ export default function DispatchFooter({
 
   const { user } = useTypedSelector((state) => state.auth);
 
+  const { updatedDispatches } = useTypedSelector((state) => state.dispatch);
+
   const dispatch = useAppDispatch();
 
-  const [saveSelections, { data, isLoading, isError }] =
-    useSaveSelectionsMutation(); // Save selections API
+  const [saveSelections, { data, isLoading }] = useSaveSelectionsMutation(); // Save selections API
+  // const [createDispatches, { isLoading }] = useCreateDispatchesMutation(); // Save selections API
+
+  const [recallInvoices, { isLoading: recalLoading }] =
+    useRecallInvoicesMutation(); // recall invoice API
 
   const handleSave = async () => {
     if (!selectedDocs.length) return;
@@ -30,13 +41,27 @@ export default function DispatchFooter({
     const payload = {
       dispatchIds: selectedDocs.map((doc) => doc.dispatchId),
       userName: user?.username || "",
+      existingDispatchNumber: updatedDispatches?.dispatchNumber || 0,
+      // existingDispatchNumber: 0,
     };
+
+    if (payload.existingDispatchNumber === 0) {
+      delete payload.existingDispatchNumber;
+    }
 
     try {
       const res = await saveSelections(payload).unwrap();
       toast.success("Successfully saved invoices!");
+      dispatch(
+        setSelectedDipatches(
+          res || {
+            dispatchNumber: 0,
+            dispatchIds: [],
+          }
+        )
+      );
+      handleRowSelect({});
       console.log(res);
-      dispatch(setSelectedDipatches(res?.updatedDispatches || []));
       if (onSubmit) {
         onSubmit(selectedDocs, selectValues);
       }
@@ -53,8 +78,77 @@ export default function DispatchFooter({
     }
   };
 
+  // const handleSave = async () => {
+  //   if (!selectedDocs.length) return;
+
+  //   const payload = {
+  //     //
+  //     userName: user?.username || "",
+  //     dispatchnum: 0,
+  //     invoices: selectedDocs.map((doc) => {
+  //       return {
+  //         // doc.dispatchId
+  //         cus_code: doc.customerCode,
+  //         saleinv_num: doc.docNo,
+  //         doctype: doc.docType,
+  //       };
+  //     }),
+  //   };
+
+  //   console.log(payload);
+
+  //   try {
+  //     const res = await createDispatches(payload).unwrap();
+  //     toast.success("Successfully saved invoices!");
+  //     // dispatch(setSelectedDipatches(res?.updatedDispatches || []));
+  // handleRowSelect({});
+  //     console.log(res);
+  //     if (onSubmit) {
+  //       onSubmit(selectedDocs, selectValues);
+  //     }
+  //     // if (onClose) onClose();
+  //   } catch (error) {
+  //     let description = "Error saving invoices. Please try again.";
+  //     if (error?.data?.errors) {
+  //       const errorMessages = Object.values(error.data.errors).flat();
+  //       if (errorMessages.length > 0) description = errorMessages.join(" ");
+  //     } else if (error?.data?.message) {
+  //       description = error.data.message;
+  //     }
+  //     toast.error("Error saving invoices!", { description, duration: 4000 });
+  //   }
+  // };
+
   const handleClose = () => {
     if (onClose) onClose();
+  };
+  // useRecallInvoicesMutation
+  const handleRecall = async () => {
+    const payload = {
+      docNo: selectedRow?.docNo || 0,
+      currentStage: 3,
+      targetStage: 2,
+      targetStatus: 3,
+      recalledBy: user?.username || "",
+    };
+
+    try {
+      const res = await recallInvoices(payload).unwrap();
+      toast.success("Successfully recalled invoices!");
+      handleRowSelect({});
+      // if (onSubmit) {
+      //   onSubmit(selectedDocs, selectValues);
+      // }
+    } catch (error) {
+      let description = "Error recalling invoices. Please try again.";
+      if (error?.data?.errors) {
+        const errorMessages = Object.values(error.data.errors).flat();
+        if (errorMessages.length > 0) description = errorMessages.join(" ");
+      } else if (error?.data?.message) {
+        description = error.data.message;
+      }
+      toast.error("Error  invoices!", { description, duration: 4000 });
+    }
   };
 
   return (
@@ -75,6 +169,14 @@ export default function DispatchFooter({
       </EditStatusDialog>
       <Button
         variant="destructive"
+        onClick={handleRecall}
+        disabled={isLoading || recalLoading || !Object.keys(selectedRow).length}
+        className="mt-1 mr-2 uppercase text-xs font-medium"
+      >
+        Recall Invoice
+      </Button>
+      <Button
+        variant="ghost"
         onClick={handleClose}
         disabled={cancelDisabled}
         className="mt-1 mr-2 uppercase text-xs font-medium"
