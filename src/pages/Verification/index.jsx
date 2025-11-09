f7c643672036f5f05a625f8c44be81c42381fbe6
Essy116 <githinjie40@gmail.com>
2025-11-09 13:37:25 +0300

    feat: add index.jsx for Store and Verification modules

---
+++ src/pages/Verification/index.jsx
@@ -0,0 +1,52 @@
+import { useTypedSelector } from "@/app/hook";
+import PageLayout from "@/components/page-layout";
+import { viewMeta } from "@/lib/utils";
+import FilterSheet from "@/pages/shared/filter-sheet/filterSheet";
+import VerificationLabelValue from "./_component/main/verification-label-value";
+import VerificationPage from "./_component/main/Verif-invoices";
+
+
+const Dispatch = () => {
+  const { user } = useTypedSelector((state) => state.auth);
+
+  let moduleArea = user["userrights"]?.map((item) => item?.moduleArea);
+  //   const pageMeta = viewMeta[moduleArea[1]?.toLowerCase() || ""];
+  const pageMeta = viewMeta["dispatch"];
+
+  const renderFilterSheet = () => {
+    return <FilterSheet />;
+  };
+
+    const renderLabelValues = () => {
+        return <VerificationLabelValue />;
+    };
+
+    const renderMainContent = () => {
+        return <VerificationPage />;
+    };
+
+    return (
+   <PageLayout
+      title={pageMeta?.title}
+      subtitle={pageMeta?.subtitle}
+      middleAction={
+        <div className="flex flex-wrap justify-center items-center gap-x-6 gap-y-2">
+          {renderLabelValues()}
+        </div>
+      }
+      rightAction={
+        <div className="flex items-center gap-2 text-sm">
+          {renderFilterSheet()}
+        </div>
+      }
+      noPadding
+      className="flex flex-col flex-1"
+    >
+      <div className="flex-1 min-h-0 overflow-hidden">
+        {renderMainContent()}
+      </div>
+    </PageLayout>
+    );
+};
+
+export default Dispatch;
